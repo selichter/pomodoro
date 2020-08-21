82314cd9ade32b19a064d48210e3fa3c1ee7568a
Sarah <lichter.sarahe@gmail.com>
2020-08-20 19:43:34 -0500

    initial commit

---
+++ Shared/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Shared
+//
+//  Created by Sarah Lichter on 8/20/20.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tests macOS/TestsMacOS.swift
@@ -0,0 +1,31 @@
+//
+//  Tests_macOS.swift
+//  Tests macOS
+//
+//  Created by Sarah Lichter on 8/20/20.
+//
+
+import XCTest
+
+class Tests_macOS: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
