1e3565704a06f420fe3a20889f3e5637ae232469
Sarah <lichter.sarahe@gmail.com>
2020-08-23 14:01:01 -0500

    pulled out button modifiers

---
+++ Shared/ButtonModifiers.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonModifiers.swift
+//  Pomodoro
+//
+//  Created by Sarah Lichter on 8/23/20.
+//
+
+import SwiftUI
+
+struct PrimaryButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content                    .foregroundColor(pomBlue)
+            .background(pomGray)
+            .cornerRadius(5.0)
+            .padding()
+    }
+}
+
